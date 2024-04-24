942575da2abc0c047f1746660e895bab52d136fd
takikomiudon <takikomiudon1743@gmail.com>
2024-04-24 16:07:05 +0900

    install tailwindcss

--- FE/src/Main.tsx
+++ FE/src/Main.tsx
@@ -1,10 +1,12 @@
-import { AppShell } from '@mantine/core'
-import React from 'react'
+import { AppShell } from "@mantine/core";
+import React from "react";
 
 const Main = () => {
   return (
-    <AppShell.Main>Main</AppShell.Main>
-  )
-}
+    <AppShell.Main>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </AppShell.Main>
+  );
+};
 
-export default Main
+export default Main;
