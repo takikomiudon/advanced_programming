d4221aaa2c614168749945ffa595babd9d54db3f
takikomiudon <takikomiudon1743@gmail.com>
2024-04-24 16:35:05 +0900

    textareaの追加

--- FE/src/Main.tsx
+++ FE/src/Main.tsx
@@ -1,10 +1,16 @@
-import { AppShell } from "@mantine/core";
-import React from "react";
+import { ActionIcon, AppShell, Textarea } from "@mantine/core";
+import { IconSend2 } from "@tabler/icons-react";
 
 const Main = () => {
   return (
-    <AppShell.Main>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <AppShell.Main className="flex flex-col justify-content-between h-full">
+      <h1 className="text-3xl font-bold grow">Chat</h1>
+      <div className="flex flex-row">
+        <Textarea className="grow"/>
+        <ActionIcon>
+          <IconSend2 />
+        </ActionIcon>
+      </div>
     </AppShell.Main>
   );
 };
